d10b1fef18c0ec399e612f1ceb56b14196df6efd
jorgemarquesferreirajunior <jorgemarquesferreirajunior@gmail.com>
2024-04-16 16:21:22 -0300

    teste01 sobreposicao

--- sobreposicao_01.py
+++ sobreposicao_01.py
@@ -1,5 +1,3 @@
-from utils import *
-import time
 import numpy as np
 import cv2
 
@@ -51,15 +49,14 @@ def testa_sobreposicao(retas_ab):
     sobrepostos = []
     for r, retangulo in enumerate(retas_ab):
         for l in range(0, 1):
-            # print(f"Retangulo[{r}]")
             for r2 in range(len(retas_ab)):
                 for l2 in range(len(retangulo)):
                     if r != r2:
                         if retas_ab[r] not in sobrepostos:   
-                            m1 = (retas_ab[r][l][3] - retas_ab[r][l][1]) / (retas_ab[r][l][2] - retas_ab[r][l][0] - 0.01)
+                            m1 = (retas_ab[r][l][3] - retas_ab[r][l][1]) / (retas_ab[r][l][2] - retas_ab[r][l][0])
                             b1 = round(retas_ab[r][l][1] - m1 * retas_ab[r][l][0])
 
-                            m2 = (retas_ab[r2][l2][3] - retas_ab[r2][l2][1]) / (retas_ab[r2][l2][2] - retas_ab[r2][l2][0]- 0.01)
+                            m2 = (retas_ab[r2][l2][3] - retas_ab[r2][l2][1]) / (retas_ab[r2][l2][2] - retas_ab[r2][l2][0])
                             b2 = round(retas_ab[r2][l2][1] - m2 * retas_ab[r2][l2][0])
                             # print(f"retas_ab[{r}][{l}] = {retas_ab[r][l]} m1={m1:.2f} b1={b1:.2f} retas_ab[{r2}][{l2}] = {retas_ab[r2][l2]} m2={m2:.2f} b2={b2:.2f}")
 
@@ -72,80 +69,62 @@ def testa_sobreposicao(retas_ab):
                                     resps_x = [x2-1, x2+1]
                                     resps_y = [y2-1, y2+1]
                                     if x1 in resps_x and y1 in resps_y:
-                                        # print(x1, y1, x2, y2)
                                         if r not in sobrepostos:
                                             sobrepostos.append(r)
                                         if r2 not in sobrepostos:
                                             sobrepostos.append(r2)
                                         break
-        # print("")
     return sobrepostos
 
-dir_home = os.getcwd()
-dir_dataset = os.path.join(dir_home, 'datasets')
-dir_printscreens = os.path.join(dir_home, 'printscreens')
-dir_resultados = os.path.join(dir_home, 'resultados')
-path_modelo = os.path.join(dir_dataset, 'runs', 'obb', 'train', 'weights/best.pt')
+coords = [
+    [[141, 376], [154, 362], [38, 253], [24, 267]],
+    [[128, 204], [144, 212], [217, 71], [201, 63]],
+    [[228, 333], [243, 332], [240, 177], [225, 177]],
+    [[269, 327], [281, 332], [343, 185], [330, 179]],
+    [[85, 261], [98, 261], [97, 110], [84, 111]],
+    [[190, 385], [209, 382], [185, 230], [166, 233]],
+    [[294, 215], [307, 211], [262, 63], [249, 67]]]
 
-detector_objetos = DetectorObjetos(path_modelo, GPU = True)
-print('----------------------------------------------------------------------')
+coords_ab = [
+    [[141, 24], [154, 38], [38, 147], [24, 133]],
+    [[128, 196], [144, 188], [217, 329], [201, 337]],
+    [[228, 67], [243, 68], [240, 223], [225, 223]],
+    [[269, 73], [281, 68], [343, 215], [330, 221]],
+    [[85, 139], [98, 139], [97, 290], [84, 289]],
+    [[190, 15], [209, 18], [185, 170], [166, 167]],
+    [[294, 185], [307, 189], [262, 337], [249, 333]]]
 
-cont = 8
-UtilitariosArquivo.esvaziar_pasta(dir_resultados)
-while True:
-    detectar = str(input("Detectar? [S/N]: ")).strip().upper()[0]
-    if detectar == 'S':
-        cont+=1
-        print(f"Deteccao [{str(cont).zfill(2)}]")
-        if cont > 49:
-            cont = 0
-        img = f"{str(cont).zfill(2)}.jpg"
-        
-        start = time.time()
 
-        imagem = cv2.imread(os.path.join(dir_printscreens, img))
-        # print(imagem.shape)
+inclinacoes = [137.03, 62.63, 91.1, 67.6, 90.38, 98.97, 106.91]
 
-        confiancas, coordenadas, centros, coords_ab, inclinacoes = detector_objetos.prever_cv2(imagem)
-        nome_img_result = f"imagem_predicao_{str(len(os.listdir(dir_resultados)) + 1).zfill(2)}.jpg"
-        path_img_result = os.path.join(dir_resultados, nome_img_result)
-
-        # CompiladorImagem.gerar_imagem_resultado(imagem, centros, coordenadas, inclinacoes, path_img_result)
-        CompiladorImagem.gerar_imagem_resultado_2(imagem, centros, coordenadas, path_img_result)
-
-        mensagem = DetectorObjetos.gerar_msg(confiancas, centros, inclinacoes)
-
-        # print("Coords_ab")
-        # print(coords_ab)
-        # print("inclinacoes")
-        # print(inclinacoes)
-        # print("Coords yolo")
-        # print(coordenadas)
-        
-        # [print(f"{str(coo):<50}-->>", coo_ab) for coo, coo_ab in zip(coordenadas, coords_ab)]
-
-        # print(mensagem)
-
-        end = time.time()
-        print(f"Tempo de Compilacao: {((end - start)*1000):.5f} milisegundos")
-
-        retas_ab = retas(coords_ab, inclinacoes)
-        sobrepostos = testa_sobreposicao(retas_ab)
-        sobrepostos = sorted(sobrepostos)
-
-        print("Retangulos Sobrepostos: ", end= "")
-        [print(s+1, end=" ") for s in sobrepostos]
-
-        print("")
-
-        for ret in coords_ab:
-            print(ret)
-        print('----------------------------------------------------------------------')
-    else:
-        break
+coords_ab2 = [[[251, 49], [262, 43], [340, 181], [329, 188]], [[82, 137], [83, 121], [241, 128], [240, 144]], [[122, 40], [137, 39], [155, 199], [141, 201]]]
+inclinacoes2 = [60.7, 2.54, 83.27]
 
 
 
 
+retas_ab = retas(coords_ab2, inclinacoes2)
+sobrepostos = testa_sobreposicao(retas_ab)
+sobrepostos = sorted(sobrepostos)
 
-    
+print("Retangulos Sobrepostos: ", end= "")
+[print(s+1, end=" ") for s in sobrepostos]
+
+print("")
+
+for ret in coords_ab2:
+    print(ret)
+# imagem_teste = np.zeros((400,400, 3), np.uint8)
+# while True:
+
+#     imagem_desenho = imagem_teste.copy()
+#     desenhar_retangulos(imagem_desenho, coords)
+
+#     cv2.imshow("imagem", imagem_teste)
+#     cv2.imshow("imagem desenho", imagem_desenho)
+
+
+#     if cv2.waitKey(0) == ord("q"):
+#         break
+
+# cv2.destroyAllWindows()
